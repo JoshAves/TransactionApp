fcc096c53ced7365efaa83f446adad87a8894a43
JoshAves <JoshuaBleuAves@Gmail.com>
2018-12-03 19:30:56 -0600

    Commit changes for Lesson 14.

--- src/main/java/edu/dcccd/trans/config/WebSecurityConfig.java
+++ src/main/java/edu/dcccd/trans/config/WebSecurityConfig.java
@@ -36,29 +36,35 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable();
+
         // The pages do not require login
         http.authorizeRequests().antMatchers("/", "/login", "/logout", "/register").permitAll();
+
         // /transaction page requires login as ROLE_USER or ROLE_ADMIN.
         // If no login, it will redirect to /login page.
         http.authorizeRequests().antMatchers("/transaction").access("hasAnyRole('ROLE_USER'," +
                 "'ROLE_ADMIN')");
+
         // For ADMIN only.
         http.authorizeRequests().antMatchers("/h2-console/**").access("hasRole('ROLE_ADMIN')");
+
         // When the user has logged in as XX.
         // But accesses a page that requires role YY,
         // AccessDeniedException will be thrown.
         http.authorizeRequests().and().exceptionHandling().accessDeniedPage("/403");
+
         // Config for Login Form
         http.authorizeRequests().and().formLogin()//
                 // Submit URL of login page.
                 .loginProcessingUrl("/j_spring_security_check") // Submit URL
                 .loginPage("/login")
-                .defaultSuccessUrl("/home")
+                .defaultSuccessUrl("/transaction")
                 .failureUrl("/login?error=true")
                 .usernameParameter("username")
                 .passwordParameter("password")
                 .and().csrf().ignoringAntMatchers("/h2-console/**")//don't apply CSRF protection /h2-console
                 .and().headers().frameOptions().sameOrigin() //allow use of frame to same origin urls
+
                 // Config for Logout Page
                 .and().logout()
                 .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
---
+++ src/main/java/edu/dcccd/trans/entity/JokeResponse.java
@@ -0,0 +1,10 @@
+package edu.dcccd.trans.entity;
+
+import lombok.*;
+
+@Data public class JokeResponse {
+    private String type;
+    private Value value;
+
+    public Value getValue() { return value; }
+}
